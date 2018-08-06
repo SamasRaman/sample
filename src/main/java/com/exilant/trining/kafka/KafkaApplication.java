447c559e7dda708932254f97e6eccfd5b59b7b7a
DHANA <DHANA@samas>
2018-08-06 21:18:48 +0530

    hii

---
+++ src/main/java/com/exilant/trining/kafka/KafkaApplication.java
@@ -0,0 +1,28 @@
+package com.exilant.trining.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KafkaApplication {
+	
+	@Autowired
+	private KafkaTemplate<String,Object> template;
+
+	private String topic="text";
+	
+	@GetMapping("/provide/{name}")
+	public String message(@PathVariable String name) {
+		template.send(topic,"Hi "+name+" Welcome to java");
+		return "Data Send";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+}
